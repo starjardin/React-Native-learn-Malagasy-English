c0af96a5a31e9d8be9d80806312b2a9ff4b69fae
AnJ <andrzej.wower@onja.org>
2021-04-20 13:25:18 +0300

    add storybook

---
+++ index.js
@@ -0,0 +1,23 @@
+// /**
+//  * @format
+//  */
+
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
+
+// AppRegistry.registerComponent(appName, () => App);
+
+// import { AppRegistry } from "react-native";
+
+// import StorybookUIRoot from "./storybook";
+
+// AppRegistry.registerComponent('learnGasy', () => StorybookUIRoot);
+
+import { AppRegistry } from "react-native";
+
+import { name as appName } from "./app.json";
+import Root from "./Root";
+
+AppRegistry.registerComponent(appName, () => Root);
+ 

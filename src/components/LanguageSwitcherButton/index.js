3bba130a00f50e69f6d55dea9ec8ed606bf54386
Tantely <tantely.and@onja.org>
2021-04-24 16:58:27 +0300

    added language switcher stories

---
+++ src/components/LanguageSwitcherButton/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+
+const styles = StyleSheet.create({
+  buttonStyles: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#06B6D4',
+    borderRadius: 30,
+    padding: 11,
+  },
+
+  textStyle: {
+    textTransform: 'uppercase',
+    fontStyle: 'normal',
+    fontSize: 13,
+    lineHeight: 16,
+    color: '#FFFFFF',
+  },
+});
+
+export default function Button({onPress, children, ...restProps}) {
+  return (
+    <TouchableOpacity style={styles.buttonStyles} activeOpacity={0.5}>
+      <Text style={[styles.textStyle, {paddingRight: 10}]}>Eng</Text>
+      {children}
+      <Text style={[styles.textStyle, {paddingLeft: 10}]}>Mal</Text>
+    </TouchableOpacity>
+  );
+}
+
+Button.defaultProps = {
+  onPress: () => {},
+  children: null,
+};
+
+Button.propTypes = {
+  onPress: PropTypes.func,
+  children: PropTypes.node,
+};

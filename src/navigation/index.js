ef486b1343772bd1748b120b7edc394227f39138
Tantely <tantely.and@onja.org>
2021-04-25 00:21:50 +0300

    set up screen navigations

---
+++ src/navigation/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import LearningScreen from '../screens/LearningSreen';
+import Home from '../screens/Home';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+
+const Stack = createStackNavigator();
+
+const CurrentListStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="LearningScreen" component={LearningScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default CurrentListStack;
+
+// yarn add @react-navigation/native
+// yarn add install react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view

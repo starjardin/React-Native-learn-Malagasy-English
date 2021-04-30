bf267e188dda2042a90d1ac56002ac03f2471684
Tantely <tantely.and@onja.org>
2021-04-30 14:52:19 +0300

    refactoring display list

--- src/screens/LearningSreen.js
+++ src/screens/LearningSreen.js
@@ -1,5 +1,5 @@
-import React, {useContext} from 'react';
-import {View, Text} from 'react-native';
+import React, {useContext, useState} from 'react';
+import {View} from 'react-native';
 
 import {LanguageContext} from '../context/globalContext';
 import ToolButton from '../components/ToolButton';
@@ -8,43 +8,9 @@ import Settings from '../icons/setting-icon.svg';
 import ChevronLeft from '../icons/chevron-left.svg';
 import LanguageSwitcherIcon from '../icons/lang-switcher';
 import {Outset, styles} from '../screens/Home';
-import PhraseTextArea from '../components/PhraseTextarea';
-import List from '../components/List';
+import Answers from '../components/ListOfAnswers/';
 
 export default ({route, navigation}) => {
-  const {item} = route.params;
-  const {state, dispatch} = useContext(LanguageContext);
-  console.log(state);
-  const {phrasesIds} = item;
-  const {phrases} = state;
-  const randomPhrasesIdsNumber = Math.floor(Math.random() * phrasesIds.length);
-  const phraseToLearn = phrases.find(
-    el => el.id === phrasesIds[randomPhrasesIdsNumber],
-  );
-
-  const randomNumb1 = Math.floor(Math.random() * phrases.length);
-  const randomNumb2 = Math.floor(Math.random() * phrases.length);
-  const randomNumb3 = Math.floor(Math.random() * phrases.length);
-  const randomNumb4 = phrases.indexOf(phraseToLearn);
-  //? Pick an object from the phrases array by their indexes.
-  const obj1 = phrases[randomNumb1];
-  const obj2 = phrases[randomNumb2];
-  const obj3 = phrases[randomNumb3];
-  const obj4 = phrases[randomNumb4];
-  const answers = [obj1, obj2, obj3, obj4];
-
-  function shuffle(sourceArray) {
-    for (var i = 0; i < sourceArray.length - 1; i++) {
-      var j = i + Math.floor(Math.random() * (sourceArray.length - i));
-
-      var temp = sourceArray[j];
-      sourceArray[j] = sourceArray[i];
-      sourceArray[i] = temp;
-    }
-    return sourceArray;
-  }
-
-  const shuffledAnswers = shuffle(answers);
   return (
     <View style={styles.container}>
       <View style={styles.buttonContainer}>
@@ -64,24 +30,8 @@ export default ({route, navigation}) => {
           <LanguageSwitcherIcon />
         </LanguageSwitcherButton>
       </View>
-      <Text>Category: {item.name.en}</Text>
-      <PhraseTextArea phrase={phraseToLearn?.name.mg} />
-      <View>
-        <Text>Pick a solution:</Text>
-        <List
-          buttonColor="red"
-          data={shuffledAnswers}
-          buttonText="pick"
-          onRowPress={
-            // console.log(event._dispatchInstances.memoizedProps.children)
-            event =>
-              dispatch({
-                type: 'answerQuestion',
-                payload: event._dispatchInstances.memoizedProps.children,
-              })
-          }
-        />
-      </View>
+
+      <Answers route={route} navigation={navigation} />
     </View>
   );
 };
